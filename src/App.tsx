b235670f62fe46a0bc1a9de420f0032a1e23966a
murash4 <singleant@gmail.com>
2021-01-13 02:19:57 +0300

    configurate todo list

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
 import { TodoList } from './components/TodoList';
-import { ITodo } from './interfaces';
+import { ITodo } from './components/interfaces';
+
+declare var confirm: (question: string) => boolean
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
@@ -18,11 +20,36 @@ const App: React.FC = () => {
     setTodos(prev => [newTodo, ...prev])
   }
 
+  const toggleHandler = (id: number) => {
+    setTodos(prev => prev.map(todo => {
+      const td = {...todo}
+
+      if (td.id === id) {
+        td.completed = !td.completed
+      }
+
+      return td
+    }))
+  }
+
+  const removeHandler = (id: number) => {
+    // const shouldRemove = window.confirm('Вы уверены, что хотите удалить элемент?')
+    const shouldRemove = confirm('Вы уверены, что хотите удалить элемент?')
+
+    if (shouldRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+  }
+
   return <>
     <Navbar />
     <div className="container">
       <TodoForm onAdd={ addHandler } />
-      <TodoList todos={ todos } />
+      <TodoList
+        todos={ todos }
+        onToggle={ toggleHandler }
+        onRemove={ removeHandler }
+      />
     </div>
   </>
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,22 @@
-import { ITodo } from "../interfaces";
+import { ITodo } from "./interfaces";
+import React from "react";
 
 type TodoListProps = {
   todos: ITodo[]
+  onToggle(id: number): void
+  onRemove: (id: number) => void
 }
 
-export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+export const TodoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove }) => {
+  if (!todos.length) {
+    return <p className="center">Пока дел нет</p>
+  }
+
+  const removehandler = (event: React.MouseEvent, id: number) => {
+    event.preventDefault()
+    onRemove(id)
+  }
+
   return (
     <ul>
       {
@@ -18,9 +30,16 @@ export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
           return (
             <li key={ todo.id } className={ classes.join(' ') }>
               <label className="todo__label">
-                <input type="checkbox" checked={ todo.completed } />
+                <input
+                  type="checkbox"
+                  checked={ todo.completed }
+                  onChange={ onToggle.bind(null, todo.id) }
+                />
                 <span className="todo__text">{ todo.title }</span>
-                <i className="todo__icon material-icons red-text">delete</i>
+                <i
+                  className="todo__icon material-icons red-text"
+                  onClick={ event => removehandler(event, todo.id) }
+                >delete</i>
               </label>
             </li>
           )
