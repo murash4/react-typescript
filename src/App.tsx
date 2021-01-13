fc4e26f4127f84eaf5ad0a19b5c87c7c12a48459
murash4 <singleant@gmail.com>
2021-01-13 22:10:40 +0300

    save todo list to Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
 import { TodoList } from './components/TodoList';
@@ -8,6 +8,16 @@ declare var confirm: (question: string) => boolean
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addHandler = (title: string) => {
     const newTodo = {
